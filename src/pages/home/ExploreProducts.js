6f225f3894add4839168efbb9889cd4751642989
Kbadr1 <kbadr.dev@gmail.com>
2021-11-14 05:24:33 +0200

    homepage update -still a big miss-

---
+++ src/pages/home/ExploreProducts.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const ExploreProducts = ({
+  exploreImage,
+  currentLanguage,
+  categories,
+  Product,
+  currentCategory,
+  setCategoryId,
+  categoryProducts,
+  products,
+}) => {
+  const slickRows = categoryProducts.length <= 3 ? 1 : 2;
+  const slickInfinite = categoryProducts.length <= 6 ? false : true;
+
+  var settings = {
+    infinite: categoryProducts.length ? slickInfinite : true,
+    rows: categoryProducts.length ? slickRows : 2,
+    slidsesPerRow: 3,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    swipeToSlide: true,
+  };
+  return (
+    <div className="explore-products">
+      <div className="container">
+        <h3>Explore products</h3>
+        <div className="row equal-cols">
+          <div className="col-md-5 p-2">
+            <div className="row ">
+              <div className="col-6 ">
+                <Link to="/shop">
+                  <img
+                    src={exploreImage}
+                    className={
+                      currentLanguage.code === "ar" ? "ar-img" : "en-img"
+                    }
+                    alt=""
+                  />
+                </Link>
+              </div>
+              <div
+                className={`col-6 list ${
+                  currentLanguage.code === "ar" ? "ar-list" : "en-list"
+                }`}
+              >
+                <ul>
+                  {categories
+                    .sort((a, b) =>
+                      currentLanguage.code === "en"
+                        ? a.en_name.localeCompare(b.en_name)
+                        : a.ar_name.localeCompare(b.ar_name)
+                    )
+                    .map((category) => (
+                      <button
+                        key={category.id}
+                        className={`btn ${
+                          currentCategory.id === category.id && "active-cat"
+                        }`}
+                        onClick={() => setCategoryId(category.id)}
+                      >
+                        <li>
+                          {currentLanguage.code === "en"
+                            ? category.en_name
+                            : category.ar_name}
+                        </li>
+                      </button>
+                    ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-7 ">
+            <Slider {...settings}>
+              {categoryProducts.length
+                ? categoryProducts.map((product) => (
+                    <div key={product.id} className="p-2">
+                      <Product product={product} />
+                    </div>
+                  ))
+                : products.map((product) => (
+                    <div key={product.id} className="p-2">
+                      <Product product={product} />
+                    </div>
+                  ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExploreProducts;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./home.css";
+import { ProductsLoading, Product } from "../../components";
+
 import { useTranslation } from "react-i18next";
 import carouselImage1 from "../../images/home/carousel-img1.jpg";
 import carouselImage2 from "../../images/home/carousel-img2.jpg";
 import featureImage1 from "../../images/home/two-features1.jpg";
 import featureImage2 from "../../images/home/two-features2.jpg";
+import exploreImage from "../../images/home/explore.jpg";
 
 import { useDispatch, useSelector } from "react-redux";
 import Offers from "./Offers";
@@ -12,6 +15,12 @@ import TwoFeatures from "./TwoFeatures";
 import Carousel from "./Carousel";
 import { productsFetch } from "../../redux/actions/productsActions";
 import TopProducts from "./TopProducts";
+import {
+  categoriesFetch,
+  categoryFetch,
+} from "../../redux/actions/categoriesActions";
+import { Link } from "react-router-dom";
+import ExploreProducts from "./ExploreProducts";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -21,6 +30,16 @@ const Home = () => {
     (state) => state.i18Reducer.currentLanguage
   );
   const products = useSelector((state) => state.productsReducer.products);
+  const productsLoading = useSelector(
+    (state) => state.productsReducer.productsLoading
+  );
+  const categories = useSelector((state) => state.categoriesReducer.categories);
+  const currentCategory = useSelector(
+    (state) => state.categoriesReducer.category
+  );
+  const categoryProducts = useSelector(
+    (state) => state.categoriesReducer.categoryProducts
+  );
 
   const [carouselText, setCarouselText] = useState("");
   const [twoFeaturesImageDir, setTwoFeaturesImageDir] = useState("");
@@ -43,7 +62,13 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(productsFetch());
+    dispatch(categoriesFetch());
   }, []);
+
+  const [categoryId, setCategoryId] = useState(null);
+  useEffect(() => {
+    dispatch(categoryFetch(categoryId));
+  }, [categoryId]);
 
   return (
     <div className="Home">
@@ -66,6 +91,17 @@ const Home = () => {
       <Offers t={t} />
       {/* top products */}
       <TopProducts t={t} products={products} />
+      {/* explore products */}
+      <ExploreProducts
+        exploreImage={exploreImage}
+        currentLanguage={currentLanguage}
+        categories={categories}
+        Product={Product}
+        currentCategory={currentCategory}
+        setCategoryId={setCategoryId}
+        categoryProducts={categoryProducts}
+        products={products}
+      />
     </div>
   );
 };
